f59a8ae4af2b8f152887358b3522e02aa470b14c
burdock <115214091+13urdock@users.noreply.github.com>
2023-07-21 17:41:34 +0900

    [D2] Title: 파스칼의 삼각형, Time: 13 ms, Memory: 13,544 KB -BaekjoonHub

--- SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.cpp
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
-
-int factorial(int n) {
-	int result = 1;
-	for (int i = 1; i <= n; i++)
-		result *= i;
-	return result;
-}
 
 int main(void) {
 	int N, T; 	//N: 삼각형의 크기(몇 줄), T: 테스트 케이스의 개수
@@ -19,7 +13,7 @@ int main(void) {
 		//삼각형 입력
 		for (int i = 0; i < N; i++) {		//N줄 반복
 			for (int j = 0; j <= i; j++) {		//i번째 줄의 숫자 입력
-				cout << factorial(i) / (factorial(i - j) * factorial(j))<< " "; //combination formula: nCr = n!/(n-r)!* r!
+				cout << tgamma(i+1) / (tgamma(i - j+1) * tgamma(j+1))<< " "; //combination formula: nCr = n!/(n-r)!* r!
 			}
 			cout << endl;
 		}
